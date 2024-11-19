b8751d7428eb30e1bb82e2628c04d7028945e579
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2024-11-18 21:14:11 -0800

    added reed switch functionality

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,21 +1,25 @@
 #define PIN_CLK 3
 #define PIN_DT 4
 #define PIN_BUTTON 5
-#define PIN_REED 6
-#define DEBOUNCE_DELAY 50
+#define PIN_REED 2
+#define DEBOUNCE_DELAY 20
 
 volatile int encoderPos = 0;
 unsigned long lastInterruptTime = 0;  // for debouncing
+volatile int counter = 0;
 
 volatile bool lastCLK = LOW;
 volatile bool lastDT = LOW;
 
-unsigned long lastStepTime = 0;  // last encoder step
-float stepsPerSecond = 0.0;
+volatile unsigned long lastReedTriggerTime = 0;
+volatile int lastReedCounter = 0;
+volatile float reedStepsPerSecond = 0.0;
+volatile bool reedSPSUpdated = false;
+volatile bool reedClosed = false;
 
 void setup() {
   // initialize serial communications
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // initialize input pins with pull-up resistors
   pinMode(PIN_CLK, INPUT_PULLUP);
@@ -26,6 +30,8 @@ void setup() {
   // interrupts for CLK and DT pins
   attachInterrupt(digitalPinToInterrupt(PIN_CLK), handleEncoderCLK, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PIN_DT), handleEncoderDT, CHANGE);
+  
+  attachInterrupt(digitalPinToInterrupt(PIN_REED), handleReedSwitch, FALLING);
 }
 
 void loop() {
@@ -36,33 +42,33 @@ void loop() {
   interrupts();
 
   // if there was at least one encoder step
-  if (pos != 0) {
-    unsigned long currentTime = micros();
-    unsigned long delta = currentTime - lastStepTime;
+  // if (pos != 0) {
+  //   Serial.println(counter);
+  // }
 
-    if (lastStepTime != 0 && delta > 0) {
-      stepsPerSecond = 1000000.0 / delta;
+  if (reedSPSUpdated) {
+    noInterrupts();
+    float sps = reedStepsPerSecond;
+    reedSPSUpdated = false;
+    interrupts();
 
-      Serial.print("steps/sec: ");
-      Serial.println(stepsPerSecond);
-    }
-
-    lastStepTime = currentTime;
+    Serial.print("Reed SPS: ");
+    Serial.println(sps);
   }
-
 }
 
-// Interrupt handler for the CLK pin
+// interrupt handler for the CLK pin
 void handleEncoderCLK() {
-  unsigned long currentTime = millis();
+  unsigned long currentTime = micros();
 
   // debounce interrupts
-  if (currentTime - lastInterruptTime > DEBOUNCE_DELAY) {
+  if (currentTime - lastInterruptTime > (DEBOUNCE_DELAY * 1000)) {
     bool currentCLK = digitalRead(PIN_CLK);
     bool currentDT = digitalRead(PIN_DT);
 
     // determine rotation
     if (lastCLK != currentCLK) {
+      counter++;
       if (currentCLK == currentDT) {
         encoderPos++;
       }
@@ -78,4 +84,30 @@ void handleEncoderCLK() {
 }
 
 void handleEncoderDT() {
+}
+
+// interrupt handler for the reed pin
+void handleReedSwitch() {
+  unsigned long currentTime = micros();
+
+  if (currentTime - lastReedTriggerTime > (DEBOUNCE_DELAY * 1000)) {
+    bool reed = digitalRead(PIN_REED);
+
+    if (reed == LOW && !reedClosed) {
+      unsigned long delta = currentTime - lastReedTriggerTime;
+
+      if (delta > 0) {
+        reedStepsPerSecond = ((counter - lastReedCounter) * 1000000) / delta;
+        reedSPSUpdated = true;
+      }
+
+      lastReedTriggerTime = currentTime;
+      lastReedCounter = counter;
+
+      reedClosed = true;
+    }
+    else if (reed == HIGH && reedClosed) {
+      reedClosed = false;
+    }
+  }
 }
